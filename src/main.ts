d0b5fc43e576bc3022bb0ef0a4b5989cfcbd3a6e
rexebin <rexebin@gmail.com>
2023-10-18 11:27:16 +0100

    add http call for versions.

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
 import * as core from '@actions/core'
-import { wait } from './wait'
+
+interface GithubPackageVersion {
+    Id: string;
+    Name: string;
+}
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+    try {
+        const org: string = core.getInput('org')
+        const packageName: string = core.getInput('packageName')
+        const token: string = core.getInput('token')
+        const response: Response = await fetch(`https://api.github.com/orgs/${org}/packages/nuget/${packageName}/versions`, {
+            method: 'GET',
+            headers: {'Authorization': 'Bearer ' + token}
+        });
+        response.json().then((data: GithubPackageVersion[]) => {
+            core.debug(JSON.stringify(data));
+            // core.setOutput(data['versions'][0], new Date().toTimeString());
+        });
+    } catch (error) {
+        // Fail the workflow run if an error occurs
+        if (error instanceof Error) core.setFailed(error.message)
+    }
 }
